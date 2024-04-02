0e29ae9ecfeb90d5300dcf4c31aee71bd483a6b2
Rita Gichimu <wangeci.gichimu@gmail.com>
2024-04-02 22:12:04 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+console.log(cats);
+
+let catslength = (cats.length)
+console.log(catslength);
+
+cats.push("Ralph");
+console.log(cats)
+
+cats.unshift ("Bob");
+console.log(cats)
+
+cats.pop()
+console.log(cats)
+
+cats.shift()
+console.log(cats)
+
+const coolcats = [...cats]
+console.log(coolcats)
+const newcats = [...coolcats,"Broom"]
+console.log(newcats)
+const bcats =["Arnold",...coolcats];
+console.log(bcats)
+
+cats.slice(-1);
+console.log(cats)
+
+cats.slice(1);
+console.log(cats)
